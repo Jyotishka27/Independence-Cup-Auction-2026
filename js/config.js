77de393a61d4c14c0edc3667a229df98b7519f6d
Jyotishka Mukhopadhyay <76933417+Jyotishka27@users.noreply.github.com>
2026-08-12 02:25:54 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,4 +8,6 @@ export const CATEGORY_LABELS = {
 
 export const REDUCTION_FACTOR = 0.8;
 export const MIN_PRICE = 10000;
+
+export const UNSOLD_FIXED_PRICE = 10000;
 export const AUTOSAVE_KEY = 'auction_autosave_v1';
